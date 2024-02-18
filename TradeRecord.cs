0a8ef4d61dc00d4718fa08d7f4a7cf4316487ba0
Leonel Nobrega <lnobrega@uma.pt>
2024-02-18 21:49:19 +0000

    Add project files.

---
+++ TradeRecord.cs
@@ -0,0 +1,10 @@
+﻿namespace TradeApp
+{
+    internal class TradeRecord
+    {
+        public string SourceCurrency { get; set; }
+        public string DestinationCurrency { get; set; }
+        public double Lots { get; set; }
+        public decimal Price { get; set; }
+    }
+}
